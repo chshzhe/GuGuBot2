58ea22e86d185a041e87bceeeabb971d0fd73cfa
chshzhe <chshzhe@126.com>
2024-01-31 01:57:54 +0800

    Add: SendMessageQueue

---
+++ utils/send_queue.py
@@ -0,0 +1,23 @@
+import asyncio
+from queue import Queue
+
+from nonebot import logger
+
+
+async def send_message():
+    while not message_queue.empty():
+        message, event, bot = message_queue.get()
+        logger.debug(f"发送消息：{message}")
+        await bot.send(event, message)
+        await asyncio.sleep(1.5)
+
+
+async def message_sender():
+    while True:
+        if not message_queue.empty():
+            await send_message()
+        else:
+            await asyncio.sleep(1.5)
+
+
+message_queue = Queue()
