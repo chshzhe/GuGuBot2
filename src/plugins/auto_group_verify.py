e74255a60b24633613e9e392673939570b399250
chshzhe <chshzhe@126.com>
2024-02-28 03:49:21 +0800

    mod: use msg_queue to send bot.set_group_add_request

--- src/plugins/auto_group_verify.py
+++ src/plugins/auto_group_verify.py
@@ -5,7 +5,7 @@ from nonebot.adapters.onebot.v11 import Bot, Event
 from nonebot.log import logger
 from nonebot.typing import T_State
 from configs.path_config import DATABASE_PATH, TEXT_PATH
-from utils import SQLiteDB
+from utils import SQLiteDB, message_queue
 from utils.permission_checker import auth_manager
 
 __plugin_name__ = "自动加群验证"
@@ -46,11 +46,13 @@ async def handle_receive(bot: Bot, event: Event, state: T_State):
                 verify_result = await verify_user(request_msg)
                 if verify_result == MODE_PASS:
                     logger.info(f"{user_id}加群{group_id}验证通过，同意入群")
-                    await bot.set_group_add_request(flag=event.flag, sub_type=event.sub_type, approve=True)
+                    # await bot.set_group_add_request(flag=event.flag, sub_type=event.sub_type, approve=True)
+                    message_queue.put(("", event, bot, "approve_add_group_require"))
                 elif verify_result == MODE_FAIL:
                     logger.info(f"{user_id}加群{group_id}验证失败，拒绝入群")
-                    await bot.set_group_add_request(flag=event.flag, sub_type=event.sub_type, approve=False,
-                                                    reason="验证失败")
+                    # await bot.set_group_add_request(flag=event.flag, sub_type=event.sub_type, approve=False,
+                    #                                 reason="验证失败")
+                    message_queue.put(("验证失败", event, bot, "reject_add_group_require"))
                 elif verify_result == MODE_WAIT:
                     logger.info(f"{user_id}加群{group_id}验证等待中")
                 await AutoVerify.finish()
@@ -62,7 +64,8 @@ async def handle_receive(bot: Bot, event: Event, state: T_State):
 async def verify_user(comment: str) -> int:
     logger.debug(f"验证信息: {comment}")
     comment_id = re.findall(r'\d{6,}', comment)[0]
-    check_result = stu_db.query(f"""SELECT * FROM example_table WHERE stuid = {comment_id}""")
+    logger.debug(f"学号: {comment_id}")
+    check_result = stu_db.query(f"""SELECT * FROM example_table WHERE stuid = '{comment_id}'""")
     logger.debug(f"查询结果: {check_result}")
     if check_result:
         if check_result[0][1] in comment:
@@ -78,7 +81,7 @@ async def get_name_id():
     stu_db.connect()
     logger.info("身份数据库已连接")
     try:
-        excel_data = pd.read_csv(stu_csv_path, encoding='utf-8')
+        excel_data = pd.read_csv(stu_csv_path, encoding='utf-8', dtype=str)
     except Exception as e:
         logger.error(f"读取csv文件失败，错误信息：{e}")
         return
--- utils/send_queue.py
+++ utils/send_queue.py
@@ -5,7 +5,7 @@ from nonebot import logger
 from nonebot.adapters.onebot.v11 import Event
 
 
-class MessageQueue():
+class MessageQueue:
     def __init__(self):
         self.message_queue = Queue()
 
@@ -14,8 +14,19 @@ class MessageQueue():
             try:
                 (message, event, bot, *args) = self.message_queue.get()
                 if isinstance(event, Event):
-                    await bot.send(event, message)
-                    logger.success(f"发送消息：{message}")
+                    if not args:
+                        await bot.send(event, message)
+                        logger.success(f"发送消息：{message}")
+                    else:
+                        if args[0] == "approve_add_group_require":
+                            await bot.set_group_add_request(flag=event.flag, sub_type=event.sub_type, approve=True)
+                            logger.success(f"同意加群请求：{event}")
+                        elif args[0] == "reject_add_group_require":
+                            await bot.set_group_add_request(flag=event.flag, sub_type=event.sub_type, approve=False,
+                                                            reason=message)
+                            logger.success(f"拒绝加群请求：{event}")
+                        else:
+                            logger.error(f"发送消息失败1：未知的event类型，{message, event, bot, args}")
                 elif isinstance(event, int):
                     if args[0] == "group":
                         await bot.send_group_msg(group_id=event, message=message)
@@ -26,7 +37,7 @@ class MessageQueue():
                     else:
                         logger.error(f"发送消息失败：未知的event类型，{message, event, bot, args}")
                 else:
-                    logger.error(f"发送消息失败：未知的event类型，{message, event, bot, args}")
+                    logger.error(f"发送消息失败2：未知的event类型，{message, event, bot, args}")
                 await asyncio.sleep(1.5)
             except Exception as e:
                 logger.error(f"发送消息失败,{e}")
@@ -41,5 +52,4 @@ class MessageQueue():
 
     def put(self, *args):
         self.message_queue.put(*args)
-        # logger.debug(f"进入队列：{**args}")
         return True
